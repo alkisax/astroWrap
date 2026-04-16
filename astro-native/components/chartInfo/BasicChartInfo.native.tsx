24196ca8a030653785cfd34cb265a050db279722
alkisax <alkisax@gmail.com>
2026-04-16 12:55:59 +0300

    style: wip basic info

--- astro-native/components/chartInfo/BasicChartInfo.native.tsx
+++ astro-native/components/chartInfo/BasicChartInfo.native.tsx
@@ -1,11 +1,14 @@
-// astro-native\components\chartInfo\BasicChartInfo.native.tsx
-import { StyleSheet, ScrollView } from 'react-native'
+// astro-native/components/chartInfo/BasicChartInfo.native.tsx
+
+import { StyleSheet, ScrollView, View } from 'react-native'
 import { useChartAnalysis } from '../../hooks/componentHooks/useChartAnalysis'
+import { colors } from '../../constants/constants'
 
 import PlanetTable from './PlanetTable'
 import ChartRuler from './ChartRuler'
 import BalanceSummary from './BalanceSummary'
 import AspectsList from './AspectsList'
+import GlassPanel from '../ui/GlassPanel'
 
 type Props = {
   data: any
@@ -19,14 +22,25 @@ const BasicChartInfo = ({ data, userOrb }: Props) => {
   } = useChartAnalysis(data, userOrb)
 
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <PlanetTable data={data} />
+    <ScrollView
+      contentContainerStyle={styles.container}
+      showsVerticalScrollIndicator={false}
+    >
+      <GlassPanel>
+        <PlanetTable data={data} />
+      </GlassPanel>
 
-      <ChartRuler data={data} />
+      <GlassPanel>
+        <ChartRuler data={data} />
+      </GlassPanel>
 
-      <BalanceSummary data={data} balance={balance} />
+      <GlassPanel>
+        <BalanceSummary data={data} balance={balance} />
+      </GlassPanel>
 
-      <AspectsList aspects={aspects} />
+      <GlassPanel>
+        <AspectsList aspects={aspects} />
+      </GlassPanel>
     </ScrollView>
   )
 }
@@ -35,8 +49,22 @@ export default BasicChartInfo
 
 const styles = StyleSheet.create({
   container: {
-    padding: 8,
-    gap: 8,
-    backgroundColor: '#000',
+    padding: 10,
+    gap: 10,
+  },
+
+  glassCard: {
+    borderRadius: 16,
+    padding: 12,
+    borderWidth: 1,
+    borderColor: 'rgba(255,255,255,0.18)',
+
+    // λίγο depth
+    shadowColor: colors.primary,
+    shadowOpacity: 0.15,
+    shadowRadius: 8,
+    shadowOffset: { width: 0, height: 2 },
+
+    elevation: 3,
   },
 })
